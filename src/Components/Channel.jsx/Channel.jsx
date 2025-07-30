79105fbf930ff683316e5af466d5b5f8f4b36585
Sonu Yadav <sy239370@gmail.com>
2025-07-30 17:46:47 +0530

    finished

--- src/Components/Channel.jsx/Channel.jsx
+++ src/Components/Channel.jsx/Channel.jsx
@@ -2,7 +2,7 @@ import styles from "./Channel.module.css";
 import Channel1 from "../../../assets/Channel1.avif";
 import Channel2 from "../../../assets/Channel2.avif";
 import Channel3 from "../../../assets/Channel3.avif";
-import Channel4 from "../../assets/Channel4.avif";
+import Channel4 from "../../../assets/Channel4.avif";
 import Channel5 from "../../../assets/Channel5.avif";
 import Channel6 from "../../../assets/Channel6.avif";
 import Channel7 from "../../../assets/Channel7.avif";
